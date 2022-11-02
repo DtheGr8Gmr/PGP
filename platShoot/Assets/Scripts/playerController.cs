c0842eb550c53660899b51d152a6c6df927f120c
Dylan <dylanjhakala@gmail.com>
2022-11-02 03:35:32 -0600

    Mostly Finalized New Movement

---
+++ platShoot/Assets/Scripts/playerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+    private bool isFacingRight = true;
+
+    private bool isGrounded = true;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public bool doubleJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if(!isFacingRight && moveInput > 0)
+        {
+            Flip();
+        }
+        else if(isFacingRight && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    void Update()
+    {
+        if(isGrounded && !Input.GetButton("Jump"))
+        {
+            doubleJump = false;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(isGrounded || doubleJump)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+                doubleJump = !doubleJump;
+            }
+        }
+
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+}
