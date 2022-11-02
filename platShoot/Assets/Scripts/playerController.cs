08c9ec26a5754fbcccbedf2a6f21bfd9e8181189
Dylan <dylanjhakala@gmail.com>
2022-11-02 15:07:53 -0600

    Finalized Movement Script and Added Functional Camera

--- platShoot/Assets/Scripts/playerController.cs
+++ platShoot/Assets/Scripts/playerController.cs
@@ -17,7 +17,11 @@ public class playerController : MonoBehaviour
     public float checkRadius;
     public LayerMask whatIsGround;
 
-    public bool doubleJump;
+    public float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+
+    public float jumpBufferTime = 0.1f;
+    private float jumpBufferCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -45,24 +49,36 @@ public class playerController : MonoBehaviour
 
     void Update()
     {
-        if(isGrounded && !Input.GetButton("Jump"))
+        if(isGrounded)
         {
-            doubleJump = false;
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
         }
 
         if(Input.GetButtonDown("Jump"))
         {
-            if(isGrounded || doubleJump)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
 
-                doubleJump = !doubleJump;
-            }
+        if(coyoteTimeCounter > 0f && jumpBufferCounter > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+            jumpBufferCounter = 0f;
         }
 
         if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+
+            coyoteTimeCounter = 0f;
         }
     }
 
